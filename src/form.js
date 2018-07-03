820de5bc111bf18303c84640e6a9fcf31f2897b9
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-02 21:52:46 -0700

    Refactored some of the JSX elements to be more responsive and more appealing.

--- src/form.js
+++ src/form.js
@@ -2,26 +2,28 @@ import React from 'react'
 
 export default function Form (props) {
   return (
-    <form id='card-form' onSubmit={props.handleSubmit} className='create-card-form col-lg-4'>
-      <div className='form-head text-center'>
-        <h1>Create a Card</h1>
-      </div>
-      <div className='form-icon text-center'>
-        <i className="fas fa-bolt"></i>
-      </div>
-      <div className='form'>
-        <div className='form-group'>
-          <label>Question</label>
-          <input type='question' className='form-control' id='questionInput' aria-describedby='questionHelp' placeholder='Enter the question...'/>
+    <div className='form-window'>
+      <form id='card-form' onSubmit={props.handleSubmit} className='create-card-form col-xl-4 col-lg-4 col-md-6 col-sm-8'>
+        <div className='form-head text-center'>
+          <h1>Create a Card</h1>
         </div>
-        <div className='form-group'>
-          <label>Answer</label>
-          <input name='answer' type='answer' className='form-control' id='answerInput' placeholder='Enter the answer...'/>
+        <div className='form-icon text-center'>
+          <i className="fas fa-bolt text-warning"></i>
         </div>
-        <div className='form-button text-center'>
-          <button type='submit' className='btn btn-success text-center'>Create Card</button>
+        <div className='form'>
+          <div className='form-group'>
+            <label>Question</label>
+            <input type='question' className='form-control' id='questionInput' aria-describedby='questionHelp' placeholder='Enter the question...'/>
+          </div>
+          <div className='form-group'>
+            <label>Answer</label>
+            <input name='answer' type='answer' className='form-control' id='answerInput' placeholder='Enter the answer...'/>
+          </div>
+          <div className='form-button text-center'>
+            <button type='submit' className='btn btn-warning text-center'>Create Card</button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   )
 }
