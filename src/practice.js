2cbe9653064376e40bff535439fcf37eb52e64d1
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-06 15:07:44 -0700

    Defined an onNextClick() method that cycles through the available flash-cards. #6

--- src/practice.js
+++ src/practice.js
@@ -10,6 +10,7 @@ export default class Practice extends React.Component {
       currentIndex: 0
     }
     this.onShowClick = this.onShowClick.bind(this)
+    this.onNextClick = this.onNextClick.bind(this)
   }
 
   onShowClick() {
@@ -18,18 +19,40 @@ export default class Practice extends React.Component {
     }))
   }
 
+  onNextClick(event) {
+    if (event.target.id === 'left') {
+      if (this.state.currentIndex > 0) {
+        this.setState({
+          currentIndex: this.state.currentIndex - 1,
+          showAnswer: false
+        })
+      }
+      else {return null}
+    }
+    if (event.target.id === 'right') {
+      if (this.state.currentIndex < this.props.card.length - 1) {
+        this.setState({
+          currentIndex: this.state.currentIndex + 1,
+          showAnswer: false
+        })
+      }
+      else {return null}
+    }
+  }
+
   render() {
     return (
       <div className='row'>
         <div className='col-xl-4 col-lg-6 col-md-8 col-sm-8 practice-card-container'>
-        <a href='#practice-cards' className='practice-right text-dark'><i id='right' className="far fa-arrow-alt-circle-right practice-right-icon"></i></a>
-        <a href='#practice-cards' className='practice-left text-dark'><i id='left' className="far fa-arrow-alt-circle-left practice-left-icon"></i></a>
+        <a onClick={this.onNextClick} href='#practice-cards' className='practice-right text-dark'><i id='right' className="far fa-arrow-alt-circle-right practice-right-icon"></i></a>
+        <a onClick={this.onNextClick} href='#practice-cards' className='practice-left text-dark'><i id='left' className="far fa-arrow-alt-circle-left practice-left-icon"></i></a>
           <div className='card practice-card'>
           <div className='show-card text-center'>
             <button onClick={this.onShowClick} className='btn btn-warning show-card-button'>show answer</button>
           </div>
             <div className='card-body'>
               <h2 className='card-text pracitce-question text-center'>{this.props.card[this.state.currentIndex].question}</h2>
+              <hr className='flash-divide col-10'></hr>
               {this.state.showAnswer ? <h4 className='card-text practice-answer text-center text-dark'>{this.props.card[this.state.currentIndex].answer}</h4> : null}
             </div>
           </div>
