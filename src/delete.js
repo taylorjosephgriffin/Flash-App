565863a43436d9e17586bb19a2b26719ab5a8f10
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-05 18:43:37 -0700

    Defined a function component Delete() that renders an alert that has the user confirm deletion. #5

---
+++ src/delete.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Delete (props) {
+  return (
+    <div className='form-window'>
+      <div id='card-form' onSubmit={props.handleSubmit} className='create-card-form col-xl-3 col-lg-6 col-md-6 col-sm-8 col-xs-4'>
+        <a className='text-dark close-form-delete' href='#card-list'><i className='far fa-times-circle text-center'></i></a>
+        <div className='form-head text-center'>
+          <h1 className='delete-head text-center'>Are you sure?</h1>
+        </div>
+        <div className='form-icon delete text-center'>
+          <i className="far fa-trash-alt text-warning"></i>
+        </div>
+        <div className='warning-text delete text-center'>
+          Cards cannot be retrieved once they are deleted.
+        </div>
+        <div className='form-button delete text-center'>
+          <button type='submit' href='#card-list' className='btn btn-warning text-center'>Delete Card</button>
+        </div>
+      </div>
+    </div>
+  )
+}
